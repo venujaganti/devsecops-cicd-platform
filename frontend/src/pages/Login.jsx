311f8bc04b2b6835ebb55d99b572fe331da841e4
venu <160247392+venujaganti@users.noreply.github.com>
2026-09-23 11:15:45 +0530

    cicd

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 
-import { useAuth } from "../hooks/useAuth";
+import useAuth from "../hooks/useAuth";
 import ErrorMessage from "../components/ErrorMessage";
 
 function Login() {
