6472ee729e8dd2adefd140b6ce2e05d7bcb37019
venu <160247392+venujaganti@users.noreply.github.com>
2026-09-23 11:27:58 +0530

    cicd

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
-export default function useAuth() {
+export function useAuth() {
   const context = useContext(AuthContext);
 
   if (!context) {
